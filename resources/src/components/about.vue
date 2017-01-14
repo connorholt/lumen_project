1a2115d8ccc8bcdfa08b84b886aa687778975d6a
dmitriy <dmitriy>
2017-01-14 20:12:26 +0300

    Добавление в докер

--- resources/src/components/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="jumbotron">
-        <h1>Коллективная проза!</h1>
-        <p>
-            Пишем рассказ вместе..
-        </p>
-        <p><a class="btn btn-default btn-lg" href="/vote" role="button">Написать свой вариант</a></p>
-    </div>
-</template>
